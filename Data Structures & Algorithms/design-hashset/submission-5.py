a09090a217a626aa35983851652c3faf96b81fc8
Leonardo Framba <89568350+leoframba@users.noreply.github.com>
2026-09-09 15:57:11 -0700

    Add: design-hashset - submission-5

---
+++ Data Structures & Algorithms/design-hashset/submission-5.py
@@ -0,0 +1,81 @@
+class MyHashSet:
+    DEFAULT_SIZE = 15
+    class HashSetNode:
+        def __init__(self, val=None, prev=None, next=None):
+            self.val = val
+            self.prev = prev
+            self.next = next
+
+
+    def __init__(self):
+        self.elements = [
+            self.HashSetNode() 
+            for _ in range(self.DEFAULT_SIZE)
+        ]
+        for head in self.elements:
+            tail = self.HashSetNode(None, head, head)
+            head.next = tail
+            head.prev = tail
+        self.size = self.DEFAULT_SIZE
+        
+
+    def add(self, key: int) -> None:
+        bucket = self._get_bucket(key)
+        tail = self._bucket_find(bucket, key)
+        if bucket.prev != tail:
+            return
+
+        # Append to the end
+        new_node = self.HashSetNode(key, tail.prev, tail)
+        # fix links
+        tail.prev = new_node
+        new_node.prev.next = new_node
+
+        return
+    
+    def _get_bucket(self, key: int) -> HashSetNode:
+        return self.elements[hash(key % self.size)]
+    
+    # returns node if found else returns tail
+    def _bucket_find(self, bucket: HashSetNode, key: int):
+        # stops when we hit tail
+        curr = bucket.next 
+        tail = bucket.prev
+        while curr != tail:
+            if curr.val == key:
+                return curr
+            curr = curr.next
+        
+        return tail
+
+    def remove(self, key: int) -> None:
+        node_to_remove = self._find(key)
+        if not node_to_remove:
+            return
+
+        # delete fix links
+        node_to_remove.prev.next = node_to_remove.next
+        node_to_remove.next.prev = node_to_remove.prev
+
+        return
+        
+    def _find(self, key: int) -> HashSetNode:
+        bucket = self._get_bucket(key)
+        tail = bucket.prev
+        node = self._bucket_find(bucket, key)
+
+        return node if node != tail else None 
+
+        
+
+    def contains(self, key: int) -> bool:
+        return self._find(key) != None
+        
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
