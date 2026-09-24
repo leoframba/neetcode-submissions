337e135c380e960c3615f2583fe730b32ea7b553
Leonardo Framba <89568350+leoframba@users.noreply.github.com>
2026-09-24 16:54:42 -0700

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if not s:
+            return True
+
+        s = "".join(c.casefold() for c in s if c.isalnum())
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
+        
